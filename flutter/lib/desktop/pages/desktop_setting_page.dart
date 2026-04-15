c1f4e418026f4d0802a1d36f9af117a8a7c69848
newsnake81 <newsnake81@dingtalk.com>
2026-04-15 08:27:24 +0800

    更新子模块地址为 newsnake81/hbb_common，定制RustDesk：修改API服务器、取消TCP等待、取消底部提示、根据分辨率自动横屏、显示隐藏连接管理窗口选项

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1223,9 +1223,9 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
             if (usePassword && !isChangePermanentPasswordDisabled())
               _SubButton('Set permanent password', setPasswordDialog,
                   permEnabled && !locked),
+            if (usePassword) radios[2],
              if (usePassword)
                hide_cm(!locked).marginOnly(left: _kContentHSubMargin - 6),
-            if (usePassword) radios[2],
           ]);
         })));
   }
